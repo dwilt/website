d5f10e873cf4e159b4f1442863314bc9edcb93da
Dan Wilt <dwilt4rville@gmail.com>
2017-03-24 11:59:47 -0700

    more style and animation tweaks

--- pages/home.js
+++ pages/home.js
@@ -6,24 +6,36 @@ import {
 
 // const createEase = () => new Ease(BezierEasing(0, 1.01, .48, 1));
 
-export default () => {
+export default async () => {
+    document.body.classList.add(`home`);
+
     const tl = new TimelineLite();
+
+    const navigation = document.querySelector(`.navigation`);
+    const navLinks = document.querySelectorAll(`.navigation a`);
+    const blurb = document.querySelector(`.blurb`);
 
     const homeLogoContainer = document.querySelector(`.home-logo-container`);
     const logo = document.querySelector(`.logo`);
-    const navigation = document.querySelector(`.navigation`);
-    const navLinks = document.querySelectorAll(`.navigation a`);
 
     homeLogoContainer.appendChild(logo);
 
     logo.classList.add(`show`);
 
     tl
-        .staggerTo(navLinks, .25, {
+        .staggerFromTo(navLinks, .5, {
             ease: Strong.easeInOut,
+            filter: `blur(20px)`,
+            transform: `translate3d(0, 3vh, 0)`,
+            opacity: 0
+        }, {
+            transform: `translate3d(0, 0, 0)`,
             filter: `blur(0)`,
-            transform: `scale(1)`,
             opacity: 1
-        }, .25);
-
+        }, .1)
+        .fromTo(blurb, 1, {
+            opacity: 0
+        }, {
+            opacity: 1
+        })
 };
